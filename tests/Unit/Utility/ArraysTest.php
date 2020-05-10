85040137132554bdef4658a7f67022c0ca973053
Frank Dekker <fdekker@gmail.com>
2020-05-10 11:57:09 +0200

    Playground cleanup

--- tests/Unit/Utility/ArraysTest.php
+++ tests/Unit/Utility/ArraysTest.php
@@ -3,9 +3,8 @@ declare(strict_types=1);
 
 namespace DigitalRevolution\SymfonyRequestValidation\Tests\Unit\Utility;
 
-use DigitalRevolution\SymfonyRequestValidation\Utility\InvalidArrayPathException;
 use DigitalRevolution\SymfonyRequestValidation\Utility\Arrays;
-use DigitalRevolution\SymfonyRequestValidation\Utility\PathFactory;
+use DigitalRevolution\SymfonyRequestValidation\Utility\InvalidArrayPathException;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -22,34 +21,5 @@ class ArraysTest extends TestCase
         $data = ['a' => []];
 
         static::assertSame(['a' => ['c']], Arrays::assignToPath($data, ['a', '0'], 'c'));
-    }
-
-    /**
-     * @covers ::findData
-     */
-    public function testFindData(): void
-    {
-        static::assertSame(['a' => 'b'], Arrays::findData(PathFactory::createFromString('a'), ['a' => 'b']));
-        static::assertSame(['a.b' => 'c'], Arrays::findData(PathFactory::createFromString('a.b'), ['a' => ['b' => 'c']]));
-        static::assertSame(['a.0' => 'b'], Arrays::findData(PathFactory::createFromString('a.#0'), ['a' => ['b']]));
-        static::assertSame(['a.0.b' => 'c'], Arrays::findData(PathFactory::createFromString('a.#0.b'), ['a' => [['b' => 'c']]]));
-
-        // key - wildcard
-        static::assertSame(
-            ['a.0' => 'b', 'a.1' => 'd'],
-            Arrays::findData(PathFactory::createFromString('a.*'), ['a' => ['b', 'd']])
-        );
-
-        // key - wildcard - key
-        static::assertSame(
-            ['a.0.b' => 'c', 'a.1.b' => 'd'],
-            Arrays::findData(PathFactory::createFromString('a.*.b'), ['a' => [['b' => 'c'], ['b' => 'd']]])
-        );
-
-        // key - wildcard - wildcard
-        static::assertSame(
-            ['a.0.b' => 'c', 'a.1.b' => 'd'],
-            Arrays::findData(PathFactory::createFromString('a.*.*'), ['a' => [['b' => 'c'], ['b' => 'd']]])
-        );
     }
 }
