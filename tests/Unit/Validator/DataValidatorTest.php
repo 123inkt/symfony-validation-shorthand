1a67718d344394297674167bc92c2c7b82b10ea5
Frank Dekker <fdekker@gmail.com>
2020-05-10 23:14:14 +0200

    RequestValidator and DataValidator test

--- tests/Unit/Validator/DataValidatorTest.php
+++ tests/Unit/Validator/DataValidatorTest.php
@@ -3,19 +3,18 @@ declare(strict_types=1);
 
 namespace DigitalRevolution\SymfonyRequestValidation\Tests\Unit\Validator;
 
-use DigitalRevolution\SymfonyRequestValidation\Validator\RequestValidator;
+use DigitalRevolution\SymfonyRequestValidation\Validator\DataValidator;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
- * @coversDefaultClass \DigitalRevolution\SymfonyRequestValidation\Validator\RequestValidator
+ * @coversDefaultClass \DigitalRevolution\SymfonyRequestValidation\Validator\DataValidator
  * @covers \DigitalRevolution\SymfonyRequestValidation\Validator\AbstractValidator
  */
-class RequestValidatorTest extends TestCase
+class DataValidatorTest extends TestCase
 {
     /**
      * @covers ::validate
@@ -24,17 +23,17 @@ class RequestValidatorTest extends TestCase
     {
         $constraint    = new NotBlank();
         $violationList = new ConstraintViolationList();
-        $request       = new Request();
+        $data          = [];
 
         /** @var ValidatorInterface|MockObject $validatorMock */
         $validatorMock = $this->createMock(ValidatorInterface::class);
         $validatorMock
             ->expects(self::once())
             ->method('validate')
-            ->with($request, $constraint)
+            ->with($data, $constraint)
             ->willReturn($violationList);
 
-        $requestValidator = new RequestValidator($constraint, $validatorMock);
-        static::assertSame($violationList, $requestValidator->validate($request));
+        $dataValidator = new DataValidator($constraint, $validatorMock);
+        static::assertSame($violationList, $dataValidator->validate($data));
     }
 }
