aefcf605f3f11a0aeadcaa2a2d918b80b7cdeab6
Frank Dekker <fdekker@gmail.com>
2020-05-12 23:07:21 +0200

    mess detector coupling suppression

--- tests/Unit/Constraint/ConstraintResolverTest.php
+++ tests/Unit/Constraint/ConstraintResolverTest.php
@@ -17,6 +17,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @coversDefaultClass \DigitalRevolution\SymfonyRequestValidation\Constraint\ConstraintResolver
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class ConstraintResolverTest extends TestCase
 {
