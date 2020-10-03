fa7cff00ff420b33dd33cb6d62f77f45205921e3
Bram Leeda <bram@123inkt.nl>
2020-10-03 22:51:44 +0200

    Add PHP 8 + Update dependencies (#26)
    Add PHP 8 to github actions
    Fix some differences with skeleton
    Include PHPUnit 8.5 and 9.4
    - Had to add suffix to FieldValidationTest dataprovider cases to get unique names

--- tests/Integration/FieldValidationTest.php
+++ tests/Integration/FieldValidationTest.php
@@ -83,23 +83,23 @@ class FieldValidationTest extends TestCase
 
         // field can't be null or empty string
         yield "required + filled: true" => ['required', 'unit test', true];
-        yield "required + filled: false" => ['required|filled', '', false];
-        yield "required + filled: false" => ['required|filled', null, false];
+        yield "required + filled: false A" => ['required|filled', '', false];
+        yield "required + filled: false B" => ['required|filled', null, false];
 
         // field must be a type of alpha
-        yield "required + alpha: true" => ['required|alpha', 'unit', true];
-        yield "required + alpha: true" => ['required|alpha', 'unitTest', true];
-        yield "required + alpha: false" => ['required|alpha', 'unit test', false];
-        yield "required + alpha: false" => ['required|alpha', 'unit-test', false];
-        yield "required + alpha: false" => ['required|alpha', 'unit5', false];
+        yield "required + alpha: true A" => ['required|alpha', 'unit', true];
+        yield "required + alpha: true B" => ['required|alpha', 'unitTest', true];
+        yield "required + alpha: false A" => ['required|alpha', 'unit test', false];
+        yield "required + alpha: false B" => ['required|alpha', 'unit-test', false];
+        yield "required + alpha: false C" => ['required|alpha', 'unit5', false];
         yield "required + alpha_dash: true" => ['required|alpha_dash', 'unit-test_9', true];
         yield "required + alpha_dash: false" => ['required|alpha_dash', 'unit test 9', false];
         yield "required + alpha_num: true" => ['required|alpha_num', 'unitTest123', true];
         yield "required + alpha_num: false" => ['required|alpha_num', 'unit-test_123', false];
 
         // field can be null or filled string, but not empty string
-        yield "required + filled + nullable: true" => ['required|filled|nullable', 'unit test', true];
-        yield "required + filled + nullable: true" => ['required|filled|nullable', null, true];
+        yield "required + filled + nullable: true A" => ['required|filled|nullable', 'unit test', true];
+        yield "required + filled + nullable: true B" => ['required|filled|nullable', null, true];
         yield "required + filled + nullable: false" => ['required|filled|nullable', '', false];
 
         // field must be within a set
@@ -124,15 +124,15 @@ class FieldValidationTest extends TestCase
 
         // field should be array
         yield "required + array: true" => ['required|array', [], true];
-        yield "required + array: false" => ['required|array', 5, false];
-        yield "required + array: false" => ['required|array', null, false];
+        yield "required + array: false A" => ['required|array', 5, false];
+        yield "required + array: false B" => ['required|array', null, false];
         yield "required + array nullable: true" => ['required|array|nullable', null, true];
 
         // field should be integer or integer castable
-        yield "required + int: true" => ['required|int', 3, true];
-        yield "required + int: true" => ['required|int', '3', true];
-        yield "required + int: false" => ['required|int', '3a', false];
-        yield "required + int: false" => ['required|int', [], false];
+        yield "required + int: true A" => ['required|int', 3, true];
+        yield "required + int: true B" => ['required|int', '3', true];
+        yield "required + int: false A" => ['required|int', '3a', false];
+        yield "required + int: false B" => ['required|int', [], false];
 
         // field should have min/max integer size
         yield "required + int + min size: true" => ['required|int|min:3', '3', true];
@@ -145,22 +145,22 @@ class FieldValidationTest extends TestCase
         yield "required + int + min+max size long: false" => ['required|int|between:3,5', '6', false];
 
         // field nullable should supersede the min:3
-        yield "required + int + min size + nullable: true" => ['required|int|nullable|min:3', 3, true];
-        yield "required + int + min size + nullable: true" => ['required|int|nullable|min:3', null, true];
+        yield "required + int + min size + nullable: true A" => ['required|int|nullable|min:3', 3, true];
+        yield "required + int + min size + nullable: true B" => ['required|int|nullable|min:3', null, true];
         yield "required + int + min size + nullable: false" => ['required|int|min:3', null, false];
 
         // field should be boolean or boolean castable
-        yield "required + bool: true" => ['required|bool', true, true];
-        yield "required + bool: true" => ['required|bool', false, true];
+        yield "required + bool: true A" => ['required|bool', true, true];
+        yield "required + bool: true B" => ['required|bool', false, true];
         yield "required + bool: false" => ['required|bool', null, false];
-        yield "required + bool + nullable: false" => ['required|bool|nullable', true, true];
-        yield "required + bool + nullable: false" => ['required|bool|nullable', null, true];
-        yield "required + bool + int: true" => ['required|bool', 1, true];
-        yield "required + bool + int: true" => ['required|bool', 0, true];
-        yield "required + bool + string: true" => ['required|bool', '1', true];
-        yield "required + bool + string: true" => ['required|bool', '0', true];
-        yield "required + bool + string: true" => ['required|bool', 'on', true];
-        yield "required + bool + string: true" => ['required|bool', 'off', true];
+        yield "required + bool + nullable: false A" => ['required|bool|nullable', true, true];
+        yield "required + bool + nullable: false B" => ['required|bool|nullable', null, true];
+        yield "required + bool + int: true A" => ['required|bool', 1, true];
+        yield "required + bool + int: true B" => ['required|bool', 0, true];
+        yield "required + bool + string: true A" => ['required|bool', '1', true];
+        yield "required + bool + string: true B" => ['required|bool', '0', true];
+        yield "required + bool + string: true C" => ['required|bool', 'on', true];
+        yield "required + bool + string: true D" => ['required|bool', 'off', true];
         yield "required + bool + string: false" => ['required|bool', 'abc', false];
 
         // field should be float or float castable
@@ -197,19 +197,19 @@ class FieldValidationTest extends TestCase
 
         // rule + constraint combination
         yield "required + rule + constraint: true" => [['required', 'string', new Assert\NotBlank()], 'test', true];
-        yield "required + rule + constraint: false" => [['required', 'string', new Assert\NotBlank()], '', false];
-        yield "required + rule + constraint: false" => [['required', 'string', new Assert\NotBlank()], 5, false];
+        yield "required + rule + constraint: false A" => [['required', 'string', new Assert\NotBlank()], '', false];
+        yield "required + rule + constraint: false B" => [['required', 'string', new Assert\NotBlank()], 5, false];
     }
 
     public function dataProviderOptionalFields(): Generator
     {
-        yield "optional: null: success" => ['string', false, false];
-        yield "optional: null: success" => ['string|nullable', null, true];
+        yield "optional: null: success A" => ['string', false, false];
+        yield "optional: null: success B" => ['string|nullable', null, true];
         yield "optional: required + string: fail" => ['required|string', false, false];
         yield "optional: string + nullable: success" => ['string|nullable', null, true];
         yield "optional: string + nullable: fail" => ['string', null, false];
-        yield "optional: string + filled: fail" => ['string|filled', null, false];
-        yield "optional: string + filled: fail" => ['string|filled', '', false];
+        yield "optional: string + filled: fail A" => ['string|filled', null, false];
+        yield "optional: string + filled: fail B" => ['string|filled', '', false];
         yield "optional: string + filled: success" => ['string|filled', 'test', true];
         yield "optional: string + nullable + filled: success A" => ['string|nullable|filled', null, true];
         yield "optional: string + nullable + filled: success B" => ['string|nullable|filled', 'test', true];
