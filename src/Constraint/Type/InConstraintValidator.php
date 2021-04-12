e1eb3d9171a1f8813ab76a93ae284aca206bbe01
Frank Dekker <fdekker@gmail.com>
2021-04-12 19:49:45 +0200

    Add coverage InConstraint + InConstraintValidator

--- src/Constraint/Type/InConstraintValidator.php
+++ src/Constraint/Type/InConstraintValidator.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
-class InValidator extends ConstraintValidator
+class InConstraintValidator extends ConstraintValidator
 {
     /**
      * @inheritDoc
