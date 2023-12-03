109a649631ca7bdd800830665ce6488868e247bd
Frank Dekker <fdekker@123inkt.nl>
2023-12-03 15:08:51 +0100

    Update symfony constraint error_names

--- src/Constraint/Type/FloatNumber.php
+++ src/Constraint/Type/FloatNumber.php
@@ -10,12 +10,10 @@ class FloatNumber extends Constraint
     public const INVALID_DECIMAL_ERROR = 'f7dfa2ce-5efa-40d4-a8ba-f02968039c28';
     public const INVALID_VALUE_TYPE    = '24e24f8a-fe44-4865-8cec-53c419ac0e87';
 
-    /** @var mixed */
-    protected static $errorNames = [
+    protected const ERROR_NAMES = [
         self::INVALID_DECIMAL_ERROR => 'INVALID_DECIMAL_ERROR',
         self::INVALID_VALUE_TYPE    => 'INVALID_VALUE_TYPE',
     ];
 
-    /** @var string */
-    public $message   = '{{ value }} is not a valid decimal.';
+    public string $message = '{{ value }} is not a valid decimal.';
 }
--- src/Constraint/Type/IntegerNumber.php
+++ src/Constraint/Type/IntegerNumber.php
@@ -10,12 +10,10 @@ class IntegerNumber extends Constraint
     public const INVALID_NUMBER_ERROR = 'fd2ba819-b3ad-4643-ae18-137817d63de9';
     public const INVALID_VALUE_TYPE   = 'af5ee700-4222-468a-8ff3-c3b394fc500b';
 
-    /** @var mixed */
-    protected static $errorNames = [
+    protected const ERROR_NAMES = [
         self::INVALID_NUMBER_ERROR => 'INVALID_NUMBER_ERROR',
         self::INVALID_VALUE_TYPE   => 'INVALID_VALUE_TYPE',
     ];
 
-    /** @var string */
-    public $message = '{{ value }} is not a valid number.';
+    public string $message = '{{ value }} is not a valid number.';
 }
